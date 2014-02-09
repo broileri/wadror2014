0f6f52e257e9f64fa1f17e83661297a31de042bf
Jenna Lindh <jelindh@cs.helsinki.fi>
2014-02-09 03:09:10 +0200

    4. viikko valmis

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,24 @@ class ApplicationController < ActionController::Base
 
   # määritellään, että metodi current_user tulee käyttöön myös näkymissä
   helper_method :current_user
-
+  helper_method :current_is_admin
+  
   def current_user
     return nil if session[:user_id].nil? 
     User.find(session[:user_id]) 
   end
 
+  def current_is_admin
+    return nil if session[:user_id].nil?
+    u = User.find(session[:user_id])
+    u.admin?
+  end
+
   def ensure_that_signed_in 
     redirect_to signin_path, notice:'you should be signed in' if current_user.nil?
+  end
+
+  def ensure_that_admin 
+    redirect_to signin_path, notice:'omg, you have no right!' if not current_user.admin?
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,8 @@ b2.beers.create name:"Huvila Pale Ale", style:"Pale Ale"
 b2.beers.create name:"X Porter", style:"Porter"
 b3.beers.create name:"Hefezeizen", style:"Weizen"
 b3.beers.create name:"Helles", style:"Lager"
+
+User.create username:"pupu", password:"AAA1", password_confirmation:"AAA1", admin:"true"
+User.create username:"lupu", password:"AAA1", password_confirmation:"AAA1", admin:"false"
+
+BeerClub.create name:"Kaniinien kaljakerho", founded:1745, city:"Helsinki"
